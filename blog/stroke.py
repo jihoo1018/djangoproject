b3879df9721ee5c720ac91ead27a9bec915f7990
jihoo1018 <wendy19991018@gmail.com>
2022-12-26 17:45:39 +0900

    2022-12-24

--- blog/stroke.py
+++ blog/stroke.py
@@ -10,9 +10,9 @@ from sklearn.model_selection import GridSearchCV
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-font_path = "C:/Windows/Fonts/malgunbd.ttf"
+'''font_path = None #"C:/Windows/Fonts/malgunbd.ttf"
 font = font_manager.FontProperties(fname=font_path).get_name()
-rc('font', family=font)
+rc('font', family=font)'''
 
 stroke_meta = {
     'id': '아이디', 'gender': '성별', 'age': '나이',
